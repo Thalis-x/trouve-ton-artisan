0634df412c39ffba2e5a3a99a8e970ff4f839ba1
thalis <sarra_borea@outlook.com>
2026-04-08 14:45:31 +0200

    Feat : Ajout de l'image de la page 404 #13

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,18 +1,44 @@
 import { Link } from 'react-router-dom';
+import image404   from '../assets/404.svg';
 
 const NotFound = () => {
   return (
-    <div className="page-404">
-      <h1>404</h1>
-      <h2 className="mb-3">Page non trouvée</h2>
-      <p className="mb-4 text-muted">
-        La page que vous avez demandée n'existe pas ou a été déplacée.
-      </p>
-      <Link to="/" className="btn btn-primary">
-        Retour à l'accueil
-      </Link>
-    </div>
-    
+    <section
+      style={{
+        backgroundImage: `url(${image404})`,
+        backgroundSize: 'contain',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor: '#6698cb',
+        minHeight: '80vh',
+        overflow: 'hidden',
+        position: 'relative',
+      }}
+      >
+  {/* Bouton centré en bas */}
+  <div style={{
+    position: 'absolute',
+    bottom: '10px',
+    width: '100%',
+    textAlign: 'center',
+  }}>
+    <Link to="/" className="btn btn-primary">
+      Retour à l'accueil
+    </Link>
+  </div>
+
+  {/* Crédit Freepik en bas à droite */}
+  <div style={{
+    position: 'absolute',
+    bottom: '10px',
+    right: '16px',
+  }}>
+    <a href="http://www.freepik.com" style={{ fontSize: '0.75rem' }} className="text-white">
+      Designed by Freepik
+    </a>
+  </div>
+</section>
+
   );
 };
 
